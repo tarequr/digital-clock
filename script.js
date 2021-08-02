1c092d1fa5e25ac9cca9cdd991b6161898b6066f
Tarequr Rahman Sabbir <tarequrr8@gmail.com>
2021-08-02 12:17:04 +0600

    create a complete digital clock app..using JS

---
+++ script.js
@@ -0,0 +1,33 @@
+function digitalClock(){
+  let date = new Date();
+
+  /*-------Time-------*/
+  let hours   = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let timeFormate = 'AM';
+
+  if (hours < 12) {
+    hours;
+  }else{
+    hours = hours - 12;
+    timeFormate = 'PM';
+  }
+
+  hours   = setValue(hours);
+  minutes = setValue(minutes);
+  seconds = setValue(seconds);
+
+  function setValue(data){
+    data = data < 10 ? '0' + data : data; //turnary operator
+    return data;
+  }
+
+  let finalTime = `${hours}:${minutes}:${seconds}`
+
+  document.getElementById('time').innerText = finalTime;
+  document.querySelector('small').innerText = timeFormate;
+
+  setInterval(digitalClock, 1000);
+}
+digitalClock();
